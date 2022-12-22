dd73e21a83eb66fa9209cc09e02fb6559a915fc2
Lilac <107518574+Satellaa@users.noreply.github.com>
2022-12-22 12:12:00 +0700

    Update c888888885.lua
    Signed-off-by: Lilac <107518574+Satellaa@users.noreply.github.com>

--- script/StarVader/c888888885.lua
+++ script/StarVader/c888888885.lua
@@ -2,7 +2,7 @@
 --Scripted by Lilac-chan
 local s,id=GetID()
 function s.initial_effect(c)
-    local e1=Effect.CreateEffect(c)
+    	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_REMOVE+CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetProperty(EFFECT_FLAG_DELAY)
