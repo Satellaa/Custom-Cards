57f12eac7f0e11699ee6905fb3192976f2b8adc2
Lucyper Regod <119866644+LucyperRegod@users.noreply.github.com>
2022-12-28 03:38:29 +0700

    Add files via upload
    Signed-off-by: Lucyper Regod <119866644+LucyperRegod@users.noreply.github.com>

---
+++ script/UnderWorldOccupation/c112326.lua
@@ -0,0 +1,68 @@
+--master planning
+
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DECKDES+CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	--e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetTarget(s.tftg)
+	e1:SetOperation(s.tfop)
+	c:RegisterEffect(e1)
+end
+
+function s.tffilter(c,tp)
+	return c:IsTrap() and (c:GetType()&0x20004)==0x20004 and not c:IsForbidden() and c:CheckUniqueOnField(tp)
+end
+
+function s.tftg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local ft=Duel.GetLocationCount(tp,LOCATION_SZONE)
+	if e:GetHandler():IsLocation(LOCATION_HAND) then ft=ft-1 end
+	if chk==0 then return ft>0 and Duel.IsExistingMatchingCard(s.tffilter,tp,LOCATION_DECK,0,1,nil,tp) end
+	--Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,0,tp,LOCATION_EXTRA)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_DECKDES,nil,0,tp,3)
+end
+function s.tfop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
+	local tc=Duel.SelectMatchingCard(tp,s.tffilter,tp,LOCATION_DECK,0,1,1,nil,tp):GetFirst()
+    if tc and Duel.MoveToField(tc,tp,tp,LOCATION_SZONE,POS_FACEUP,true) then
+        local e1=Effect.CreateEffect(e:GetHandler()) --negate effect
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_DISABLE)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1,true)
+		local e2=Effect.CreateEffect(e:GetHandler())
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_DISABLE_EFFECT)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e2,true)
+        
+        --aux.DelayedOperation(tc,PHASE_END,id,e,tp,function(ag) Duel.Destroy(ag,REASON_EFFECT) end,nil,0)
+    end
+    local e1=Effect.CreateEffect(e:GetHandler())
+    e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+    e1:SetCode(EVENT_PHASE+PHASE_END)
+    e1:SetCountLimit(1)
+  --  e1:SetReset(RESET_PHASE+PHASE_END)
+    e1:SetCondition(s.tdcon)
+    e1:SetOperation(s.tdop)
+    Duel.RegisterEffect(e1,tp)
+
+
+ --   function s.tdfilter(c)
+   --     return c
+    --end
+
+end
+
+function s.tdcon(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.IsExistingMatchingCard(nil,tp,LOCATION_SZONE,LOCATION_SZONE,1,nil)
+end
+function s.tdop(e,tp,eg,ep,ev,re,r,rp)
+    local g=Duel.GetMatchingGroup(s.tdfilter,tp,LOCATION_SZONE,LOCATION_SZONE,nil)
+    Duel.SetLP(tp,Duel.GetLP(tp)-#g*1000)
+   -- Duel.SendtoDeck(g,nil,0,REASON_EFFECT)
+end
