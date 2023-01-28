32fbabdb7566cd1d2c35a7ba91c1be4e566ae20a
Lilac <Lilac>
2023-01-28 18:45:49 +0700

    Add "Tierkreis" cards

---
+++ script/Tierkreis/c2100000005.lua
@@ -0,0 +1,101 @@
+-- Zodic Zygel
+-- Scripted by Lilac
+local s,id=GetID()
+function s.initial_effect(c)
+    -- To Deck
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DAMAGE+CATEGORY_TODECK)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,id)
+	e1:SetCost(s.tdcost)
+	e1:SetTarget(s.tdtg)
+	e1:SetOperation(s.tdop)
+	c:RegisterEffect(e1)
+	-- Draw
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_DRAW)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_TO_GRAVE)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetCondition(s.drwcon)
+	e2:SetCost(s.drwcost)
+	e2:SetTarget(s.drwtg)
+	e2:SetOperation(s.drwop)
+	c:RegisterEffect(e2)
+end
+s.listed_series={0xf11}
+function s.cfilter(c)
+	return c:IsSetCard(0xf11) and c:IsDiscardable() and not c:IsCode(id)
+end
+function s.tdcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsDiscardable()
+		and Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_HAND,0,1,e:GetHandler()) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DISCARD)
+	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_HAND,0,1,1,e:GetHandler())
+	e:SetLabelObject(g:GetFirst())
+	g:AddCard(e:GetHandler())
+	Duel.SendtoGrave(g,REASON_DISCARD+REASON_COST)
+end
+function s.tdfilter(c)
+	return c:IsSetCard(0xf11) and c:IsMonster() and not c:IsAttribute(ATTRIBUTE_WIND) and c:IsAbleToDeck()
+end
+function s.tdtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.tdfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,tp,LOCATION_GRAVE)
+end
+function s.tdop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local sg=Duel.SelectMatchingCard(tp,s.tdfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	if #sg>0 and Duel.SendtoDeck(sg,nil,SEQ_DECKTOP,REASON_EFFECT)~=0 then
+		local attcheck=e:GetLabelObject()
+		if attcheck:IsAttribute(ATTRIBUTE_WIND) and attcheck:IsSetCard(0xf11) then
+			Duel.BreakEffect()
+			Duel.Damage(1-tp,500,REASON_EFFECT)
+  	end
+   end
+end
+function s.drwcon(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+	return c:IsReason(REASON_COST+REASON_DISCARD) and not c:IsReason(REASON_RULE) and re:GetHandler():IsSetCard(0xf11) and re:GetHandler()~=e:GetHandler() and re:GetHandler()~=e:GetHandler()
+end
+function s.drwcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetActivityCount(tp,ACTIVITY_SUMMON)==0
+		and Duel.GetActivityCount(tp,ACTIVITY_SPSUMMON)==0 and Duel.GetActivityCount(tp,ACTIVITY_NORMALSUMMON)==0 end
+	-- Cannot Normal Summon/Set or Special Summon monsters
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetDescription(aux.Stringid(id,2))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CANNOT_SUMMON)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH+EFFECT_FLAG_CLIENT_HINT)
+	e1:SetTargetRange(1,0)
+	Duel.RegisterEffect(e1,tp)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	Duel.RegisterEffect(e2,tp)
+	local e3=e1:Clone()
+	e3:SetCode(EFFECT_CANNOT_MSET)
+	Duel.RegisterEffect(e3,tp)
+end
+function s.drwtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(1)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+function s.drwop(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	if Duel.Draw(p,d,REASON_EFFECT)~=0 then
+		local c=e:GetHandler()
+		if c:IsReason(REASON_COST+REASON_DISCARD) and re:GetHandler():IsSetCard(0xf11) and re:GetHandler():IsAttribute(ATTRIBUTE_WIND) and re:GetHandler()~=e:GetHandler() and re:GetHandler()~=e:GetHandler() then
+			Duel.BreakEffect()
+			Duel.Draw(tp,1,REASON_EFFECT)
+   end
+  end
+ end
