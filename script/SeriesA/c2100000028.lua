3e2495b984f2868863b275940ef6ed29a13caf1f
Nana <Nana>
2023-03-29 13:20:08 +0700

    Fixed something

--- script/SeriesA/c2100000028.lua
+++ script/SeriesA/c2100000028.lua
@@ -1,24 +1,23 @@
 --Grea's Ember
 --Amatuer Script by Lucyper Regod <(")
+Duel.LoadScript("custom_constant.lua")
 local s,id=GetID()
-
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,1))
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetHintTiming(TIMING_ATTACK,TIMINGS_CHECK_MONSTER_E+TIMING_ATTACK)
-	--e1:SetCost(s.cost)
 	e1:SetCountLimit(1,id)
-	e1:SetTarget(s.target)
-	e1:SetOperation(s.activate)
+	e1:SetTarget(s.destg)
+	e1:SetOperation(s.desop)
 	c:RegisterEffect(e1)
 	--Search
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,2))
+	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e2:SetType(EFFECT_TYPE_QUICK_O)
 	e2:SetCode(EVENT_FREE_CHAIN)
@@ -30,59 +29,47 @@ function s.initial_effect(c)
 	e2:SetOperation(s.thop)
 	c:RegisterEffect(e2)
 end
-
-s.listed_series={SET_AZURIST} --always treat as a "Azurist" card
-
---fillter Send.
-
-function s.sdfilter(c)
-    return c:IsSetCard(SET_AZURIST) and c:IsLocation(LOCATION_DECK+LOCATION_EXTRA) 
+s.listed_series={SET_AZURIST}
+s.listed_names={2100000024,2100000028}
+function s.tgfilter(c)
+    return c:IsSetCard(SET_AZURIST) and c:IsLocation(LOCATION_DECK|LOCATION_EXTRA) 
 		and not c:IsCode(2100000024) and not c:IsCode(2100000028) and c:IsAbleToGrave()
 end
-
-function s.thfilter(c)
-	return c:IsSetCard(SET_AZURIST) and c:IsMonster()
-end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc) --cần ít nhất 1 card trong deck hoặc extra, 1 card trên sân trừ lá này.
-    local g = Duel.GetMatchingGroup(s.sdfilter,tp,LOCATION_DECK+LOCATION_EXTRA,0,nil)
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsOnField() and chkc~=e:GetHandler() end
-	if chk==0 then return Duel.IsExistingTarget(nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,e:GetHandler()) and #g>0 end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_DECK|LOCATION_EXTRA,0,1,nil) 
+		and Duel.IsExistingTarget(nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,e:GetHandler()) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 	local g=Duel.SelectTarget(tp,nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,e:GetHandler())
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
-	Duel.SetPossibleOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK|LOCATION_EXTRA)
 end
-
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) then 
-		local sdg = Duel.GetMatchingGroup(s.sdfilter, tp, LOCATION_DECK+LOCATION_EXTRA,0, nil) --send group
+	if tc and tc:IsRelateToEffect(e) then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-		local sd = sdg:Select(tp, 1, 1,nil) --select 1 card from send group
-		if Duel.SendtoGrave(sd , REASON_EFFECT) then --send and if you do,...
+		local g=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_DECK|LOCATION_EXTRA,0,1,1,nil)
+		if Duel.SendtoGrave(g,REASON_EFFECT)~=0 then
 			Duel.Destroy(tc,REASON_EFFECT)
 		end
 	end
 end
-
+function s.thfilter(c)
+	return c:IsSetCard(SET_AZURIST) and c:IsMonster() and c:IsAbleToHand()
+end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
-
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
-	--add and place
-	if #g>0 then
-		Duel.SendtoHand(g,nil,REASON_EFFECT)
+	if #g>0 and Duel.SendtoHand(g,nil,REASON_EFFECT)>0 and g:GetFirst():IsLocation(LOCATION_HAND) then
 		Duel.ConfirmCards(1-tp,g)
 		Duel.ShuffleHand(tp)
-		local g2=Duel.SelectMatchingCard(tp,Card.IsAbleToDeck,tp,LOCATION_HAND,0,1,1,nil)
-		if #g2>0 then
-			Duel.BreakEffect()
-			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-			Duel.SendtoDeck(g,nil,SEQ_DECKBOTTOM,REASON_EFFECT)
-		end
+		Duel.BreakEffect()
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+		local td=Duel.SelectMatchingCard(tp,Card.IsAbleToDeck,tp,LOCATION_HAND,0,1,1,nil)
+		Duel.SendtoDeck(td,nil,SEQ_DECKBOTTOM,REASON_EFFECT)
 	end
 end	
