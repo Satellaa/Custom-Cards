559558c875942983299b3ec04ef3ba4156a21480
Nana <Nana>
2023-04-20 20:46:08 +0700

    .

--- script/Starrytail/c999999991.lua
+++
@@ -1,6 +0,0 @@
---Scorching Managram
---Scripted by Lilac-chan
-local s,id=GetID()
-function s.initial_effect(c)
-	Ritual.AddProcEqual(c,aux.FilterBoolFunction(Card.IsAttribute,ATTRIBUTE_FIRE))
-end
